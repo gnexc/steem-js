3bf21d4064aaf4a4af35f3decad23da4575cc0a6
Gnexcoin <97846937+gnexcoin@users.noreply.github.com>
2022-02-15 18:37:41 +0600

    Update memo.js

--- src/auth/memo.js
+++ src/auth/memo.js
@@ -94,8 +94,8 @@ function checkEncryption() {
     let plaintext;
     encodeTest = true // prevent infinate looping
     try {
-      const wif = '5JdeC9P7Pbd1uGdFVEsJ41EkEnADbbHGq6p1BwFxm6txNBsQnsw'
-      const pubkey = 'STM8m5UgaFAAYQRuaNejYdS8FVLVp9Ss3K1qAVk5de6F8s3HnVbvA'
+      const wif = '5JtkpDwfL1uajHvXy57V7yyQNBdvPqqvaZUnZkcSFBNzuaorXQh'
+      const pubkey = 'GNX8RkNHhiX6s3mupG3UZKQagwZ69yvbhy3tTPCoAf8Yi97VN4fjS'
       const cyphertext = encode(wif, pubkey, '#memo爱')
       plaintext = decode(wif, cyphertext)
     } catch(e) {
